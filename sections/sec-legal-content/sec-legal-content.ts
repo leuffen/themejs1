087966ca59180c7eebc8b9a36ce4894aeb9355e7
Matthias Leuffen <matthes@leuffen.de>
2023-07-20 11:17:09 +0200

    fix legal content

--- sections/sec-legal-content/sec-legal-content.ts
+++ sections/sec-legal-content/sec-legal-content.ts
@@ -5,10 +5,10 @@ Joda.registerTemplate("sec-legal-content",
     // language=HTML
     `
 
-        <div class="tjs__sec-legal-content">
+        <div class="tjs__sec-legal-content :: mobile :lg:">
             <div class="out-wrapper">
-                <div class="container">
-                    <div class="inner-wrapper">
+                <div class="container p-0">
+                    <div class="inner-wrapper :: p-4 :lg: p-5">
                             <div class="tjs__section-text">
                                 <slot></slot>
                             </div>
