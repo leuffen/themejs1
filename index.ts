e4c8d10e2d4b016f36c5d5d809caa48c5e091279
Matthias Leuffen <matthes@leuffen.de>
2023-08-03 17:05:34 +0200

    update

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-import {StyleEditorElement} from "./src.dev/StyleEditorElement";
-
-export * from "./src/index";
-export * from "./src.dev/StyleEditorElement";
+import "@leuffen/jodastyle";
 export * from "./sections/sections";
 export * from "./elements/elements";
 export * from "@leuffen/isl-base";
 
-if (location.hostname === "localhost") {
-    console.log("Running in development mode");
-    document.body.append(new StyleEditorElement());
-}
