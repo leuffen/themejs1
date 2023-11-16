bdeb26c7e1e6d57a931a29db8b20f2b514fe63d0
Matthias Leuffen <matthes@leuffen.de>
2023-11-16 11:47:50 +0100

    update

---
+++ sections/infobox/infobox.ts
@@ -0,0 +1,18 @@
+import {Joda} from "@leuffen/jodastyle";
+
+
+Joda.registerTemplate("infobox",
+    // language=HTML
+    `
+
+        <div class="tjs-infobox alert bg-soft-[[layout.type]] d-flex mb-3" tabindex="0">
+            <i style="width: 48px; height: 48px" class="text-[[layout.type]] me-3 bi [[layout.icon]] h1"></i>
+            <div>
+                <slot></slot>
+
+
+            </div>
+        </div>
+    `, {
+    type: "success", icon: "bi-info-circle-fill"
+    });
