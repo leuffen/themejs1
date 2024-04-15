43b96fe8cf8dc66b1fff63a1c0f272a2d0485997
Matthias Leuffen <matthes@leuffen.de>
2024-04-15 11:57:09 +0200

    add multilanguage support

---
+++ sections/navbar-switch2/navbar-switch2.ts
@@ -0,0 +1,47 @@
+
+// language=HTML
+import {Joda, JodaDescriptionManager} from "@leuffen/jodastyle";
+
+const html = `
+
+    <nav class="isl-navbar-switch2 :: mobile :xl:">
+
+        <div class="isl-navbar-switch2__top-bar container-xxl">
+            <slot data-select="ul.navbar-top"></slot>
+        </div>
+        <div class="isl-navbar-switch2__nav">
+            <div class="container-xl">
+                <div class="isl-navbar-switch2__nav--inner">
+                    <div class="isl-navbar-switch2__nav--logo">
+                        <slot data-select=".brand"></slot>
+                    </div>
+                    <div class="isl-navbar-switch2__nav--items">
+                        <slot data-select="ul.navbar-main" data-child-class="isl-navbar-switch2__nav--items-item"></slot>
+                    </div>
+                    <div class="isl-navbar-switch2__nav--mobile-menu :: :xl: d-none" >
+                        <div class="isl-navbar-switch2__nav--mobile-menu-selector" onclick="this.closest('nav').classList.toggle('sidebar')">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <aside>
+            <div class="backdrop" onclick="this.closest('nav').classList.remove('sidebar')"></div>
+            <div class="menu">
+                <div class="menu-header">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16"  onclick="this.closest('nav').classList.remove('sidebar')">
+                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+                    </svg>
+                </div>
+                <slot data-select="ul.navbar-main" data-copy="1" data-child-class="isl-navbar-switch2__nav--items-item"></slot>
+            </div>
+        </aside>
+    </nav>
+
+`;
+
+Joda.registerTemplate("navbar-switch2", html);
