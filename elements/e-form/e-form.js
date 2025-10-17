9d5bafc7f705e0d3d7ea3edb4c283c5069d316db
Matthias <m@tth.es>
2025-10-17 12:23:22 +0200

    sync

--- elements/e-form/e-form.js
+++
@@ -1,8 +0,0 @@
-import { Joda } from "@leuffen/jodastyle";
-Joda.registerTemplate("e-form", 
-// language=HTML
-`
-        <form class="tjs__e-form" data-micx-formmail-preset="default" data-micx-formmail-sent-message="E-Mail erfolgreich gesendet!">
-            <slot></slot>
-        </form>
-    `);
